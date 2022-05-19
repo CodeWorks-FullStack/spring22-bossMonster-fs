4c7302a72bc17871de778586ade88af3cf7586fd
Harrison Wheeler <wheelerharrison93@gmail.com>
2022-05-19 11:57:55 -0600

    this actually makes sense now

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ let heroes = {
   terryTate: {
     name: "Triple T",
     health: 100,
-    damage: 10,
-    quantity: 0,
+    damage: 2,
+    quantity: 1,
     img: 'https://f1.media.brightcove.com/8/694922499001/694922499001_4874436235001_4874324610001-vs.jpg?pubId=694922499001&videoId=4874324610001'
   },
   masterChief: {
     name: "The Chief",
     health: 100,
-    damage: 15,
-    quantity: 0,
+    damage: 5,
+    quantity: 1,
     img: 'https://i0.wp.com/www.kakuchopurei.com/wp-content/uploads/2021/11/Become-Master-Chief-Halo-Infinite.jpg?fit=1920%2C1080&ssl=1'
   },
   harrington: {
     name: "Code Monkey",
     health: 100,
     damage: 1,
-    quantity: 0,
+    quantity: 1,
     img: './harrisonheadshot.jpeg'
   }
 }
@@ -25,16 +25,10 @@ let heroes = {
 let boss = {
   name: "jake",
   health: 100,
-  damage: .5
+  damage: 85
 }
 
-function addHero(heroName) {
-  let hero = heroes[heroName]
-  hero.quantity++
-  // console.log('hero obj', hero)
-  attackBoss()
-  setInterval(attackBoss, 2000)
-}
+// STRETCH GOAL would be to add additional heroes
 
 function attackBoss() {
   for (let key in heroes) {
@@ -51,13 +45,27 @@ function attackBoss() {
   attackHeroes()
 }
 
+function healHero(key) {
+  let hero = heroes[key]
+  hero.health += 10
+  drawHeroHealth(hero)
+}
+
 function attackHeroes() {
+  setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * Object.keys(heroes).length)
+    let keys = Object.values(heroes)
+    let randomHero = keys[randomIndex]
+    randomHero.health -= 15
+    drawHeroHealth(randomHero)
+  }, 1000)
   // NOTE this is not necessary for the checkpoint!!!!
-  let randomIndex = Math.floor(Math.random() * Object.keys(heroes).length)
-  let keys = Object.values(heroes)
-  let randomHero = keys[randomIndex]
-  randomHero.health -= boss.health
   // Would need to make sure we are only redrawing heroes health bars, not entire templates
+}
+
+function drawHeroHealth(hero) {
+  // @ts-ignore
+  document.getElementById(`hero-${hero.name}`).style = `width: ${hero.health}%`
 }
 
 function drawHeroes() {
@@ -70,11 +78,11 @@ function drawHeroes() {
         src="${hero.img}"
         class="img-fluid" alt="">
       <div class="progress my-2">
-        <div class="progress-bar" role="progressbar" style="width: ${hero.health}%;" aria-valuenow="25" aria-valuemin="0"
+        <div class="progress-bar" id="hero-${hero.name}" role="progressbar" style="width: ${hero.health}%;" aria-valuenow="25" aria-valuemin="0"
           aria-valuemax="100"></div>
       </div>
       <div class="d-flex justify-content-center mt-2">
-        <button class="btn btn-danger" onclick="addHero('${key}')">${hero.name}</button>
+        <button class="btn btn-success" onclick="healHero('${key}')">Heal</button>
       </div>
    </div>
      `
@@ -85,6 +93,7 @@ function drawHeroes() {
 function drawBossHealth() {
   let elem = document.getElementById('boss-health')
   console.log(elem);
+  // @ts-ignore
   document.getElementById('boss-health').style = `width: ${boss.health}%`
   if (boss.health <= 0) {
     document.getElementById('boss').classList.add('bg-danger')
@@ -92,3 +101,4 @@ function drawBossHealth() {
 }
 
 drawHeroes()
+attackHeroes()
